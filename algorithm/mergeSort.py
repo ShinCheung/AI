982d5ed8625054ac8a5757180ad9388ae4236618
ShinCheung <42176146+ShinCheung@users.noreply.github.com>
2018-11-07 15:08:21 +0800

    Update mergeSort.py

--- algorithm/mergeSort.py
+++ algorithm/mergeSort.py
@@ -1,29 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 def mergeSort(alist):
-    if len(alist) > 1:
-        mid = len(alist)//2
-        lefthalf = alist[:mid]
-        righthalf = alist[mid:]
-
-        mergeSort(lefthalf)
-        mergeSort(righthalf)
-
-        i, j, k = 0, 0, 0
-        while i < len(lefthalf) and j < len(righthalf):
-            if lefthalf[i] < righthalf[j]:
-                alist[k] = lefthalf[i]
-                i += 1
-            else:
-                alist[k] = righthalf[j]
-                j += 1
-            k += 1
-
-        while i < len(lefthalf):
-            alist[k] = lefthalf[i]
-            i += 1
-        while j < len(righthalf):
-            alist[k] = righthalf[j]
-            j += 1
-    return alist
+    if len(alist) <= 1:
+        return alist
+    num = len(alist)//2
+    left = mergeSort(alist[:num])
+    right = mergeSort(alist[num:])
+    return merge(left, right)
+ 
+ 
+def merge(left, right):
+    l, r = 0, 0
+    result = []
+    while l < len(left) and r < len(right):
+        if left[l] < right[r]:
+            result.append(left[l])
+            l += 1
+        else:
+            result.append(right[r])
+            r += 1
+    result.extend(left[l:])
+    result.extend(right[r:])
+    return result
 
 alist = [54,26,93,17,77,31,44,55,20]
 print(mergeSort(alist))
