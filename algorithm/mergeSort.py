68ed4e2c28dab1bfcdf28909a326e7ad0ea616fc
ShinCheung <42176146+ShinCheung@users.noreply.github.com>
2018-11-07 14:56:03 +0800

    Update mergeSort.py

--- algorithm/mergeSort.py
+++ algorithm/mergeSort.py
@@ -23,7 +23,7 @@ def mergeSort(alist):
         while j < len(righthalf):
             alist[k] = righthalf[j]
             j += 1
+    return alist
 
 alist = [54,26,93,17,77,31,44,55,20]
-mergeSort(alist)
-print(alist)
+print(mergeSort(alist))
