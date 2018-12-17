7c00e380838efe4be2fcdff55a448437d1fc9fb2
ShinCheung <42176146+ShinCheung@users.noreply.github.com>
2018-12-17 17:03:12 +0800

    Update FindContinuousSequence.py

--- algorithm/FindContinuousSequence.py
+++ algorithm/FindContinuousSequence.py
@@ -4,24 +4,22 @@
 # 和为s的连续正数序列
 
 def FindContinuousSequence(tsum):
-        if tsum < 3:
-            return []
-        small=1
-        big=2
-        middle=(1+tsum)/2
-        curSum=small+big
-        output=[]
-        while small < middle:
-            if curSum == tsum:
-                output.append(list(range(small,big+1)))
-                big += 1
-                curSum += big
-            elif curSum >= tsum:
-                curSum -= small
-                small += 1
-            else:
-                big += 1
-                curSum += big
-        return output
+    if tsum < 3:
+        return []
+    small, big = 1, 2
+    curSum = small + big
+    res = []
+    while small < big:
+        if curSum == tsum:
+            res.append(list(range(small,big + 1)))
+            big += 1
+            curSum += big
+        elif curSum >= tsum:
+            curSum -= small
+            small += 1
+        else:
+            big += 1
+            curSum += big
+    return res
 
 print(FindContinuousSequence(15))
